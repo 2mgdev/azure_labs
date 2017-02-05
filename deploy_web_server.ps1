b759ce42b63de88574546ff417a33099f66e3700
2mgdev <info@2mgsolutions.com>
2017-02-05 07:24:26 +0000

    3

--- deploy_web_server.ps1
+++ deploy_web_server.ps1
@@ -1,7 +1,8 @@
 $folder = "c:\temp"
-$log = "c:\temp\AzureLog.txt"
+$log = "c:\temp\deploy_web_server.txt"
 $date = get-date
 $HttpPort=80
+$httpRule="web server access rule port 80"
 
 
 function Add-FirewallException
@@ -9,10 +10,10 @@ function Add-FirewallException
     param([string] $port)
 
     # Delete an exisitng rule
-    netsh advfirewall firewall delete rule name="Windows Remote Management (HTTPS-In)" dir=in protocol=TCP localport=$port
+    netsh advfirewall firewall delete rule name=all dir=in protocol=TCP localport=$port >> $log
 
     # Add a new firewall rule
-    netsh advfirewall firewall add rule name="Windows Remote Management (HTTPS-In)" dir=in action=allow protocol=TCP localport=$port
+    netsh advfirewall firewall add rule name=$httpRule dir=in action=allow protocol=TCP localport=$port >> $log
 }
 
 
@@ -24,8 +25,8 @@ if (!(Test-Path $log)) {
     New-Item -Path $log -ItemType File
     Add-Content -Value "NEW LOG: Azure PowerShell Endpoint - $date" -Path $log
 
-    Install-WindowsFeature -Name Web-Server
-    Install-WindowsFeature -Name Web-Mgmt-Tools
+    Install-WindowsFeature -Name Web-Server >> $log
+    Install-WindowsFeature -Name Web-Mgmt-Tools >> $log
 
     Add-FirewallException -port $HttpPort
 
